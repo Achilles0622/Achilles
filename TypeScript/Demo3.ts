54686c974944e12a31318da19d5d88fd1ea097d0
mt <08096561996g@gmail.com>
2023-11-24 10:37:59 +0900

    no message

---
+++ TypeScript/Demo3.ts
@@ -0,0 +1,13 @@
+const counts :number =918;
+const  myName :string='jspang';
+const XiaoJJ:{
+    name:string,
+    age:number
+}={name:'大脚 ',
+ age:18
+}
+const xiaoJJs:string[]=['谢大脚','刘英','小红','123']
+
+class Person{}
+const dajiao:Person=new Person()
+const jianXiaoJieJie:()=>string=()=>{return '大脚'}
