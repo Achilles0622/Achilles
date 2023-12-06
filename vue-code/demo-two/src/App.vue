0eec71cf16f163143570bfa167be78a8842dcbec
mt <08096561996g@gmail.com>
2023-12-06 13:41:16 +0900

    vue

---
+++ vue-code/demo-two/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="app-container">
+    <h1 ref="myh1">App根组件</h1>
+    <button @click="showThis">打印this</button>
+    <button @click="onReset">重置0</button>
+    <hr/>
+    <button @click="comName='Left'">展示 Left</button>
+    <button @click="comName='Right'">展示 Right</button>
+
+    <div class="box">
+      
+      <keep-alive>
+        <component :is="comName" :key="comName"></component>
+      </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+import Left from '@/components/Left.vue'
+import Right from '@/components/Right.vue'
+
+export default {
+  data() {
+    return {
+      comName: 'Left'
+    };
+  },
+  methods: {
+    showThis() {
+      console.log(this.$refs.myh1);
+      this.$refs.myh1.style.color = 'red';
+    },
+    onReset() {
+      // 如果 Left 组件具有 resetCount 方法，可以调用
+      // this.$refs.comLeft.resetCount();
+    },
+    created(){
+      console.log('Left---ok')
+    },
+    destroyed(){
+      console.log('left---no')
+    }
+  },
+ 
+  components: {
+    Left,
+    Right
+  }
+}
+</script>
+
+<style lang="less">
+.app-container {
+  padding: 1px 20px 20px;
+  background-color: #954848;
+}
+
+.box {
+
+}
+</style>
---
+++ vue-code/demo-two/src/components/Right.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="right-container">
+      <h3>Right 组件 ---{{ count }}</h3>
+      <button @click="count += 1">+1</button>
+      <button @click="resetCount">重置</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        count: 0
+      };
+    },
+    methods: {
+      resetCount() {
+        this.count = 0;
+      }
+    }
+  };
+  </script>
+  
+  <style lang="less">
+  .right-container {
+    padding: 0 20px 20px;
+    background-color: rgb(120, 164, 109);
+    min-height: 250px;
+    flex: 1;
+  }
+  </style>
+  
