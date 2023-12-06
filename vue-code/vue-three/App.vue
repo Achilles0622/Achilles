938aaf214ab41084e7cc9b6e87004d8bfeb47b92
mt <08096561996g@gmail.com>
2023-12-06 20:50:34 +0900

    no message

---
+++ vue-code/vue-three/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="app-container">
+    <h1 v-color="color">App根组件</h1>
+    <p v-color="'red'">color</p>
+    <hr />
+    <Aricle>
+      <template v-slot:title>
+        <h3>一首诗</h3>
+      </template>
+      <template #content="{ msg }">
+        <div>
+          <p>啊，大海，全都是水 </p>
+          <p>啊，蜈蚣，全都是腿 </p>
+          <p>1231231231231231231</p>
+          <p>{{ scope.msg }}</p>
+          <p>{{ user.name }}</p>
+        </div>
+      </template>
+      <template #author>
+        <div>
+          作者：缤果果
+        </div>
+      </template>
+    </Aricle>
+
+    <hr>
+    <div class="box" style="display: none;">
+      <Left>
+        <template v-slot:default>
+          <p>ppppppppp</p>
+        </template>
+      </Left>
+    </div>
+  </div>
+</template>
+<script>
+import Left from '@/components/Left.vue'
+import Right from '@/components/Right.vue'
+import Aricle from '@/components/Article.vue'
+export default {
+  data() {
+    return {
+      color: 'blue'
+    }
+  },
+  components: {
+    Left,
+    Right,
+    Aricle
+  },
+  directives: {
+    color: {
+      bind(el, binding) {
+        console.log('v-color!!!!!!!!')
+        el.style.color = binding.value
+        console.log(binding)
+      }
+    }
+  }
+}
+</script>
+<style lang="less">
+.app-container {
+  padding: 1px 20px 20px;
+  background-color: #7e9548;
+}
+
+.box {}
+</style>
---
+++ vue-code/vue-three/Article.vue
@@ -0,0 +1,44 @@
+<template>
+<div class ="article-container">
+<div class="header-box">
+    <slot name="title"></slot>
+</div>
+<div class="content-box">
+    <slot name="content" msg="hello vue.js" :user="userinfo"></slot>
+</div> 
+<div class="footer-box">
+    <slot name="author"></slot>
+     
+</div>
+</div>
+</template>
+<script> 
+export default{
+name:'Article',
+data(){
+    return{
+        userinfo:{
+            name:'zhangsan',
+            age:20
+        }
+    }
+}
+}
+</script>
+<style lang="less" scoped>
+.article-container{
+    >div{
+        min-height: 150px;
+    }
+    .header-box{
+        background-color: pink;
+    }
+    .content-box{
+        background-color: lightblue;
+    }
+    .footer-box{
+        background-color: lightsalmon;
+    }
+}
+
+</style>
